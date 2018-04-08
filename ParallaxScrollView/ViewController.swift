d7682dee25ba69d0b3ae8bcf822bfdc9e8c72143
李腾飞 <litengfei@winkind-tech.com>
2018-04-08 10:07:56 +0800

    Initial Commit

---
+++ ParallaxScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxScrollView
+//
+//  Created by LiTengFei on 2018/4/8.
+//  Copyright © 2018年 WinKind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
